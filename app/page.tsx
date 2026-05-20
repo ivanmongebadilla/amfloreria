bab28d3c78f6d323ebf247f885aa0d529481101b
Ivan Monge <ivanmongebadilla@gmail.com>
2026-05-20 13:56:30 -0700

    Fix components folder directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { Navbar } from "@/components/shared/Navbar";
-import { Footer } from "@/components/shared/Footer";
+import { Navbar } from "@/src/components/shared/Navbar";
+import { Footer } from "@/src/components/shared/Footer";
 
 const collections = [
   {

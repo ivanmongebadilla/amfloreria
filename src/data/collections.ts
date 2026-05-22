11ec2b59fea1e988657270da7579d7144cb7937d
Ivan Monge <ivanmongebadilla@gmail.com>
2026-05-22 11:52:08 -0700

    Added real images to collections and categories data arrays

--- src/data/collections.ts
+++ src/data/collections.ts
@@ -1,4 +1,7 @@
 import romance1 from "../../public/images/romance1.jpg";
+import luxury1 from "../../public/images/luxury.jpg"
+import events1 from "../../public/images/events1.png"
+import seasonal2 from "../../public/images/seasonal2.jpg"
 
 export const collections = [
   {
@@ -9,19 +12,16 @@ export const collections = [
   {
     title: "Luxury",
     description: "Arreglos premium sofisticados.",
-    image:
-      "https://images.unsplash.com/photo-1563241527-3004b7be0ffd?q=80&w=1200&auto=format&fit=crop",
+    image: luxury1.src,
   },
   {
     title: "Eventos",
     description: "Diseños para bodas y celebraciones.",
-    image:
-      "https://images.unsplash.com/photo-1519378058457-4c29a0a2efac?q=80&w=1200&auto=format&fit=crop",
+    image: events1.src,
   },
   {
     title: "Seasonal",
     description: "Inspiradas en la temporada.",
-    image:
-      "https://images.unsplash.com/photo-1490750967868-88aa4486c946?q=80&w=1200&auto=format&fit=crop",
+    image: seasonal2.src,
   },
 ];

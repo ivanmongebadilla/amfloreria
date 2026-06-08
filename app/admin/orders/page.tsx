c507dbb0ca8eaa529d8625cccf86402e297d17aa
Ivan Monge <ivanmongebadilla@gmail.com>
2026-06-08 11:03:48 -0700

    Added force dynamic

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -5,6 +5,8 @@ import { OrdersStats } from "@/src/components/admin/OrderStats";
 import { getAllOrders } from "@/src/actions/orders/getAllOrders";
 import OrderPage from "@/src/features/orders/OrderPage";
 
+export const dynamic = "force-dynamic";
+
 export default async function Orders() {
 
     const orders = await getAllOrders()
